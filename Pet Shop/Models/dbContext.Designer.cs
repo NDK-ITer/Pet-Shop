a22977f649e2aa47d74aebfb8b8b191aa51c6b3b
LuciferDarkonster <trantrungbaokha@gmail.com>
2023-03-23 12:29:24 +0700

    login Form

--- Pet Shop/Models/dbContext.Designer.cs
+++ Pet Shop/Models/dbContext.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Schools\Web\Dead Line Pet Shop\Khuong\Pet Shop\Models\dbContext.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\DATA\HocTap\LapTrinhWeb\Gib\Pet-Shop\Pet Shop\Models\dbContext.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
