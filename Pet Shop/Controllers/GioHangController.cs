ced843a1b4b92ddd4b6db3c253fd213a49368a99
NDK-ITer <116534879+NDK-ITer@users.noreply.github.com>
2023-03-31 09:34:00 +0700

    cập nhập giỏ hàng

---
+++ Pet Shop/Controllers/GioHangController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Ajax.Utilities;
+using Pet_Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+using PagedList;
+
+namespace Pet_Shop.Controllers
+{
+    public class GioHangController : Controller
+    {
+        private QuanLyThuCungEntities dbContext ;
+        public GioHangController()
+        {
+            dbContext = new QuanLyThuCungEntities();
+        }
+        public List<SPGioHang> LayGiohang()
+        {
+            List<SPGioHang> gioHangs = Session["GioHang"] as List<SPGioHang>;
+            if (gioHangs == null)
+            {
+                gioHangs = new List<SPGioHang>();
+                Session["GioHang"] = gioHangs;
+            }
+            return gioHangs;
+        }
+
+        // GET: GioHang
+        //[Authorize]
+        public ActionResult GioHang(int? page, int? pageSize)
+        {
+            if (page == null) { page = 1; }
+            if (pageSize == null) { pageSize = 2; }
+            List<SPGioHang> gioHangs = LayGiohang();
+            ViewBag.TongSL = TongSL();
+            ViewBag.TongThanhTien = TongTien();
+            if (gioHangs.Count == 0)
+            {
+                return View(gioHangs.ToPagedList((int)page, (int)pageSize));
+            }
+            
+            return View(gioHangs.ToPagedList((int)page, (int)pageSize));
+        }
+
+        public ActionResult ThemVaoGioHang(string id, string strURL)
+        {
+            List<SPGioHang> gioHangs = LayGiohang();
+            var sanPham = gioHangs.FirstOrDefault(n => n.MaDD == id);
+            if (sanPham == null)
+            {
+                sanPham = new SPGioHang(id);
+                gioHangs.Add(sanPham);
+                return Redirect(strURL);
+            }
+            else
+            {
+                sanPham.SoLuong++;
+                return Redirect(strURL);
+            }
+        }
+
+        public ActionResult XoaGioHang(string id)
+        {
+            List<SPGioHang> gioHangs = LayGiohang();
+            SPGioHang sanPham = gioHangs.FirstOrDefault(n => n.MaDD == id);
+            if (sanPham != null)
+            {
+                gioHangs.Remove(sanPham);
+                return RedirectToAction("GioHang");
+            }
+            return RedirectToAction("GioHang");
+        }
+
+        private int TongSL()
+        {
+            int result = 0;
+            List<SPGioHang> gioHangs = LayGiohang();
+            if (gioHangs == null)
+            
+            {
+                return result;
+            }
+            return gioHangs.Count;
+        }
+
+        private double TongTien()
+        {
+            double result = 0;
+            List<SPGioHang> gioHangs = LayGiohang();
+            if (gioHangs.Count > 0)
+            {
+                result = gioHangs.Sum(n => n.ThanhTien);
+                return result;
+            }
+            return result;
+        }
+    }
+}
---
+++ Pet Shop/Models/SPGioHang.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace Pet_Shop.Models
+{
+    public class SPGioHang
+    {
+        private QuanLyThuCungEntities db = new QuanLyThuCungEntities();
+        public string MaDD { get; set; }
+        public string TenDD { get; set; }
+        public double DonGia { get; set; }
+        public int SoLuong { get; set; }
+        public string AnhDaiDien { get; set; }
+        public double ThanhTien { get { return DonGia * SoLuong; } }
+
+        public SPGioHang(string maDD)
+        {
+            MaDD = maDD;
+            DoDungTC doDungTC = (DoDungTC)db.DoDungTCs.FirstOrDefault(n => n.MaDD == maDD);
+            TenDD = doDungTC.TenDD;
+            AnhDaiDien = doDungTC.DoiTuongKD.AnhDaiDien;
+            DonGia = (double)doDungTC.DoiTuongKD.DonGia;
+            SoLuong = 1;
+        }
+    }
+}
